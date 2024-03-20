93ba600764db703b717e2e8a9b34d42311fc2d23
Ahmedsaleh1234 <a12661826@gmail.com>
2024-03-20 23:43:50 +0200

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Minimum Operations
+"""
+
+
+def minOperations(n):
+    """
+    Function: that calculates the fewest number of operations
+    n: exactly n H characters in the file.
+    Return: The sum of operation
+    """
+    if n < 2:
+        return 0
+    save_op = []
+    i = 1
+    while n != 1:
+        i += 1
+        if n % i == 0:
+            while n % i == 0:
+                n = n / i
+                save_op.append(i)
+    return sum(save_op)
