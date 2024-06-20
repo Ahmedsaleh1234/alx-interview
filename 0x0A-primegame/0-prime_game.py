07fbdb9a396ddcaac23fb7a101f33b3534efbe14
Ahmedsaleh1234 <a12661826@gmail.com>
2024-06-21 01:17:35 +0300

    prime_game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""Program that performs prime game"""
-
 
 def isWinner(x, nums):
     """Function that performs prime game"""
-    player1 = 0
-    player2 = 0
-    if not nums or x < 1:
-        return None
-    for i in nums:
-        if (i % 2 == 0 or i == 1) and (i != 2 or i != 3):
-            player2 += 1
+    player1_wins = 0
+    player2_wins = 0
+    for n in nums:
+        prime_count = sum(1 for i in range(2, n + 1) if all(i %
+                          j != 0 for j in range(2, int(i**0.5) + 1)))
+        if prime_count % 2 == 0:
+            player2_wins += 1
         else:
-            player1 += 1
-    if player1 > player2:
-        return 'Maria'
+            player1_wins += 1
+    if player1_wins > player2_wins:
+        return "Maria"
+    elif player2_wins > player1_wins:
+        return "Ben"
     else:
-        return 'Ben'
-    return None
+        return None
