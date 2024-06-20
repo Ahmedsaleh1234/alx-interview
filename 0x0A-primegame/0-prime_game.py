3c9865ac4a15d753b81f098a49d65fd6d8d221ef
Ahmedsaleh1234 <a12661826@gmail.com>
2024-06-21 01:10:19 +0300

    prime_game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -9,7 +9,7 @@ def isWinner(x, nums):
     if not nums or x < 1:
         return None
     for i in nums:
-        if (i % 2 == 0 or i == 1) and i != 2:
+        if (i % 2 == 0 or i == 1) and (i != 2 or i != 3):
             player2 += 1
         else:
             player1 += 1
@@ -17,3 +17,4 @@ def isWinner(x, nums):
         return 'Maria'
     else:
         return 'Ben'
+    return None
--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -3,4 +3,4 @@
 isWinner = __import__('0-prime_game').isWinner
 
 
-print("Winner: {}".format(isWinner(3, [2, 2, 1])))
+print("Winner: {}".format(isWinner(8, [2, 5, 1, 4, 3, 15, 10])))
